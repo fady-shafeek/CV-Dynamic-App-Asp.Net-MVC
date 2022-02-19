b20d85fcb3f9a672cb169266cdaa29e7ba606c1a
Fady Shafeek <Fady.Shafeek@hotmail.com>
2022-02-20 01:31:17 +0200

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskDay3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Exprience()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Interests()
+        {
+            return View();
+        }
+        public ActionResult Awards()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskDay3.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+    }
+}
